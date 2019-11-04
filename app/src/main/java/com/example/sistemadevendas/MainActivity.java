80b1a0890350e259911a4ab140c5243f133bdbf2
Dieinimy Maganha <dieinimy@gmail.com>
2019-11-04 20:43:42 -0300

    Criando validações de usuários

--- app/src/main/java/com/example/sistemadevendas/MainActivity.java
+++ app/src/main/java/com/example/sistemadevendas/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sistemadevendas;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,10 +11,38 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     EditText ed1, ed2;
+    Button b1, b2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ed1 = findViewById(R.id.user);
+        ed2 = findViewById(R.id.pass);
+
+        b1 = findViewById(R.id.btn1);
+        b2 = findViewById(R.id.btn2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+    }
+
+    public void Login() {
+        String username = ed1.getText().toString();
+        String password = ed2.getText().toString();
+
+        if (username.equals("") || password.equals("")) {
+            Toast.makeText(this, "FAVOR DIGITAR AS INFORMAÇOES CORRETAS", Toast.LENGTH_SHORT).show();
+        } else if (username.equals("san") && password.equals("123")) {
+            Toast.makeText(this, "SEJA BEM VINDO AO SISTEMA", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "USUÁRIO OU SENHA NÃO CORRESPONDEM", Toast.LENGTH_SHORT).show();
+        }
     }
 }
