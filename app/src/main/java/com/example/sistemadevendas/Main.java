02f3ba0ad0361696bd1371dcf338a65004f4e6fb
Dieinimy Maganha <dieinimy@gmail.com>
2019-11-05 21:26:36 -0300

    Criado redirecionamento para pagina de categorias

--- app/src/main/java/com/example/sistemadevendas/Main.java
+++ app/src/main/java/com/example/sistemadevendas/Main.java
@@ -2,13 +2,28 @@ package com.example.sistemadevendas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends AppCompatActivity {
+     Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.btn1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirecionando a pagina
+                Intent i = new Intent(Main.this, Categoria.class);
+                startActivity(i);
+            }
+        });
     }
 }
