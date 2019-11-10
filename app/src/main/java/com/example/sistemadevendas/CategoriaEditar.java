5187a5c04efee6fd67e8563be8fa669ed1217451
Dieinimy Maganha <dieinimy@gmail.com>
2019-11-10 10:19:33 -0300

    Criado metodo para exibir os dados coletados da visualização da categoria

--- app/src/main/java/com/example/sistemadevendas/CategoriaEditar.java
+++ app/src/main/java/com/example/sistemadevendas/CategoriaEditar.java
@@ -2,7 +2,9 @@ package com.example.sistemadevendas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,5 +24,22 @@ public class CategoriaEditar extends AppCompatActivity {
         b1 = findViewById(R.id.btn1);
         b2 = findViewById(R.id.btn2);
         b3 = findViewById(R.id.btn3);
+
+        Intent i = getIntent();
+
+        String id = i.getStringExtra("id").toString();
+        String categoria = i.getStringExtra("categoria").toString();
+        String desc = i.getStringExtra("catdesc").toString();
+
+        ed1.setText(id);
+        ed2.setText(categoria);
+        ed3.setText(desc);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
