80ce16457361d35a5513e1cfc930575292356e99
Dieinimy Maganha <dieinimy@gmail.com>
2019-11-10 10:47:35 -0300

    Criando metodo para editar uma categoria

--- app/src/main/java/com/example/sistemadevendas/CategoriaEditar.java
+++ app/src/main/java/com/example/sistemadevendas/CategoriaEditar.java
@@ -2,11 +2,15 @@ package com.example.sistemadevendas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class CategoriaEditar extends AppCompatActivity {
     EditText ed1, ed2, ed3;
@@ -38,8 +42,31 @@ public class CategoriaEditar extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Editar();
             }
         });
+    }
+
+    public void Editar() {
+        try {
+            String catid = ed1.getText().toString();
+            String categorianome = ed2.getText().toString();
+            String catdescricao = ed3.getText().toString();
+            SQLiteDatabase db = openOrCreateDatabase("supervenda",
+                    Context.MODE_PRIVATE, null);
+            String sql = "UPDATE categoria SET categoria = ?, catdesc = ? WHERE id = ?";
+            SQLiteStatement statement = db.compileStatement(sql);
+            statement.bindString(1, categorianome);
+            statement.bindString(2, catdescricao);
+            statement.bindString(3, catid);
+            statement.execute();
+            Toast.makeText(this, "Categoria Atualizada com sucesso", Toast.LENGTH_SHORT).show();
+
+            Intent i = new Intent(getApplicationContext(), Main.class);
+            startActivity(i);
+
+        } catch (Exception ex) {
+            Toast.makeText(this, "Categoria deu pau!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
